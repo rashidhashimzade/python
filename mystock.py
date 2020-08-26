5ad5366fe035657a73c3a7bad053486ca030741e
Rashid Hashimzade <59527891+rashidhashimzade@users.noreply.github.com>
2020-08-26 18:45:35 +0400

    Update mystock.py
    sehvin duzelisi

--- mystock.py
+++ mystock.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 21 21:08:59 2020
+Created on Wed Aug 26 15:52:36 2020
 
-@author: Rashid
+@author: Rashid hashimzade
+
 """
 
-# malin girisi
-a = input (" Mallarin sayini daxil edin : ")
-b = int (a)
-
-# sayi deyeri ve adi daxil edirik.
+a = input ("Mallarin sayini daxil edin : ")
+b = int(a)
 
 myst = []
 
@@ -19,37 +17,38 @@ for num in range(b):
     cnt = int(input(" Malin sayini daxil edin : "))
     myst.append([name, cnt, price, cnt*price])
 
-    
 print (myst)
 
-
-# en boyuk deyeri tapiram
 
 max_mal = myst[0][3]
 max_ad_mal = myst[0][0]
 for mal in myst :
-    if mal[3] > max_mal :
-        max_mal = mal[3]
-        max_ad_mal = mal[0]
-        
+   if mal[3] > max_mal :
+       max_mal = mal[3]
+       max_ad_mal = mal[0]
+      
 print("En deyerli malim : " + max_ad_mal + " deyeri = "+str(max_mal))
-   
-    
 
-#satiram
+
 satilan_name = input ("Aldigiz esyani daxil edin : ")
 satilan_say = input ("Aldigiz esyanin sayini daxil edin : ")
 c7 = int (satilan_say)
 tapdim = False
+n = 0
 
 for mal in  myst :
-  if (satilan_name == myst[0][0] ):
+  if (satilan_name == myst[n][0]):
       tapdim = True
-      print("dogru") 
-      if (c7 < myst[0][3] ):
-          tapdim = True
+      if (c7 < myst[0][1] ) :
+          myst[n][1] = myst[n][1] - c7
           print("dogru")
-      
+      else :
+           print(" Yeterli say yoxdur. ")
+  n = n + 1
+  
+    
 if (tapdim == False ) :
     print("bu maldan yoxdur. ")
+
+print(myst)
     
